d0d51e85b11d37a76dfa23ebd45cb615525cba52
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-14 14:17:34 +0300

    Rename the "descript" module to "describe"

--- printo/__init__.py
+++ printo/__init__.py
@@ -1,7 +1,7 @@
-from printo.descript import (
+from printo.describe import (
     describe_data_object as describe_data_object,
 )
-from printo.descript import (
+from printo.describe import (
     descript_data_object as descript_data_object,
 )
 from printo.filters import not_none as not_none
