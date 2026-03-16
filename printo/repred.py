359bbe77fd64c9241eab2f07105b6996a863d6f8
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-16 18:45:13 +0300

    Handle self.name when positional-only args are used

--- printo/repred.py
+++ printo/repred.py
@@ -18,7 +18,10 @@ def get_mapping(cls: ClassType) -> Dict[str, str]:
 
     tree = parse(source)
 
-    self_name = tree.body[0].args.args[0].arg
+    try:
+        self_name = tree.body[0].args.args[0].arg
+    except IndexError:
+        self_name = tree.body[0].args.posonlyargs[0].arg
 
     results = {}
     for node in tree.body[0].body:
